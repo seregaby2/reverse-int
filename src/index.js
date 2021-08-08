936985fb0c3b98ab5a780414989766fd098acd2c
seregaby2 <serega.by@inbox.ru>
2021-08-09 00:02:13 +0300

    feat: add solution task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+   let num = Math.abs(n);
+        let numStr = num.toString();
+        let result = '';
+        let remainder;
+        for(let i = 0; i < numStr.length; i++) {
+            remainder = num % 10;
+            num = Math.floor(num / 10); 
+            result = `${result}${remainder}`
+        }
+        return result;
+    };
+
